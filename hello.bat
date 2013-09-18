bbbfa6138231732add09e244f608eba1405ef925
Peter Kirkpatrick <peter.k143@gmail.com>
2013-09-18 11:10:32 -0400

    added some neat tricks

--- hello.bat
+++ hello.bat
@@ -1,5 +1,13 @@
 :: hello world bitches!
 @ECHO off
+GOTO :START
+
+moar comments
+
+:START 
 CLS
+::@SET when=%date:/=% :: rm the "/" delimiter
+@SET WHEN=%date%
 ECHO Hello World!
+ECHO %WHEN%
 PAUSE
