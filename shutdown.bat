b1975cb3776a3f11fd6e142b270430ebe99e8015
Peter Kirkpatrick <peter.k143@gmail.com>
2013-09-18 11:23:25 -0400

    added shutdown stuffs

---
+++ shutdown.bat
@@ -0,0 +1,8 @@
+@ECHO off
+
+shutdown -r :: restart
+shutdown -s :: shutdown
+shutdown -l :: logoff
+shutdown -t xx :: number of seconds to wait
+shutdown -i :: interactive
+shutdown -a :: aborts previous command 
