13635e557139d2a887e2ac183751768b45af96da
Peter Kirkpatrick <peter.k143@gmail.com>
2015-11-02 23:16:10 -0500

    change - to / and add force restart

--- shutdown.bat
+++ shutdown.bat
@@ -1,8 +1,9 @@
 @ECHO off
 
-shutdown -r :: restart
-shutdown -s :: shutdown
-shutdown -l :: logoff
-shutdown -t 02 :: wait 2 seconds
-shutdown -i :: interactive
-shutdown -a :: aborts previous command 
+shutdown /r :: restart
+shutdown /s :: shutdown
+shutdown /l :: logoff
+shutdown /t 02 :: wait 2 seconds
+shutdown /i :: interactive
+shutdown /a :: aborts previous command
+shutdown /f /r /t 0 :: force restart immediately
